e276a77666398ac31ad7c207af7e377020a0751e
Magdalena <menovska3@yahoo.com>
2020-04-28 23:16:05 +0200

    new car description

---
+++ app/src/main/java/com/android/gsixacademy/kotlinbasictutrorial/CarDetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.android.gsixacademy.kotlinbasictutrorial
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CarDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_car_details)
+    }
+
+}
--- app/src/main/java/com/android/gsixacademy/kotlinbasictutrorial/RecycleViewActivity.kt
+++ app/src/main/java/com/android/gsixacademy/kotlinbasictutrorial/RecycleViewActivity.kt
@@ -12,18 +12,19 @@ class RecycleViewActivity:AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycleview)
 
-        listCars.add(Car ("McLaren", "MC description", R.drawable.mclaren))
-        listCars.add (Car("Audi", "Audi description", R.drawable.audi_r))
-        listCars.add(Car("Honda", "Honda description", R.drawable.honda_nsx))
-        listCars.add(Car("Jaguar", "Jaguar description", R.drawable.jaguar_f))
-        listCars.add (Car("Aston Martin", "Aston Martin description", R.drawable.aston_martin))
-        listCars.add(Car("Maserati", "Maserati description", R.drawable.maserati_granturismo))
-        listCars.add (Car("McLaren", "McLaren description", R.drawable.mclaren))
-        listCars.add(Car("Mercedes", "Mercedes description", R.drawable.mercedes_amg))
-        listCars.add(Car("Nissan", "Nissan description", R.drawable.nissan ))
+        listCars.add(Car ("McLaren", "The McLaren 720S is a sports car designed and manufactured by British automobile manufacturer McLaren Automotive. It is the second all-new car in the McLaren Super Series, replacing the 650S beginning in May 2017.\n" +
+                "\n" +
+                "The 720S was launched at the Geneva Motor Show on 7 March 2017 and is built on a modified carbon monocoque, which is lighter and stiffer in contrast to the 650S.", R.drawable.mclaren))
+        listCars.add (Car("Audi", "The Audi R8[2] is a mid-engine, 2-seater sports car,[2][3] which uses Audi's trademark quattro permanent all-wheel drive system.[2] It was introduced by the German car manufacturer Audi AG in 2006.", R.drawable.audi_r))
+        listCars.add(Car("Honda", "The Honda NSX (second generation), marketed as the Acura NSX in North America and China, is a 2-seater, all-wheel drive, mid-engine hybrid sports car developed and manufactured by Honda in the United States. It succeeds the original NSX that was produced in Japan from 1990 to 2005.", R.drawable.honda_nsx))
+        listCars.add(Car("Jaguar", "The Jaguar F-Type (X152) is a two-door, two-seater sports car, manufactured by British luxury car manufacturer Jaguar Land Rover under their Jaguar Cars marque since 2013. \n" + "\n" + "The car was launched initially as a 2-door soft-top convertible, with a 2-door fastback coupé version launched in 2013.", R.drawable.jaguar_f))
+        listCars.add (Car("Aston Martin", "The Aston Martin DB11 is a grand tourer[2] produced by British luxury car manufacturer Aston Martin since 2016. It debuted at the Geneva Motor Show in March 2016 as a replacement to the DB9.[3] It is the first model launched in Aston Martin's 'second century' plan and the first car launched since the company's tie-up with Daimler AG.", R.drawable.aston_martin))
+        listCars.add(Car("Maserati", "The Maserati GranTurismo and GranCabrio (Tipo M145) are a series of a grand tourers produced by the Italian automobile manufacturer Maserati from 2007 to 2019. ... The GranTurismo set a record for the most quickly developed car in the auto industry, going from design to production stage in just nine months.", R.drawable.maserati_granturismo))
+        listCars.add(Car("Mercedes", "The Mercedes-AMG GT (C190 / R190) is a sports car produced in coupé and roadster bodystyles by German automobile manufacturer Mercedes-AMG. ... After the SLS AMG, it is the second sports car developed entirely in-house by Mercedes-AMG.", R.drawable.mercedes_amg))
+        listCars.add(Car("Nissan", "The Nissan GT-R is a high-performance sports car and grand tourer produced by Nissan, which was unveiled in 2007. It is the successor to the Skyline GT-R, although no longer part of the Skyline range itself, that name now being used for Nissan's luxury-sport market.", R.drawable.nissan ))
 
         for (i in 0..100){
-            listCars.add(Car("Nissan", "Nissan description"+i, R.drawable.nissan ))
+            listCars.add(Car("Nissan", "The Nissan GT-R is a high-performance sports car and grand tourer produced by Nissan, which was unveiled in 2007. It is the successor to the Skyline GT-R, although no longer part of the Skyline range itself, that name now being used for Nissan's luxury-sport market." +i, R.drawable.nissan ))
         }
 
 
